93f5336e34a5b5161ad5a60d000d50c780fa2d73
Caio-H3nr1que <121584527+Caio-H3nr1que@users.noreply.github.com>
2025-11-17 21:10:57 -0300

    teste

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    '**/*.js',
+    '!**/node_modules/**',
+    '!**/coverage/**',
+    '!jest.config.js'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  testMatch: [
+    '**/*.test.js',
+    '**/*.integration.test.js'
+  ]
+};
